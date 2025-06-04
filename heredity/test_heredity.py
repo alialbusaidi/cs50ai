817a76e1177d612264852dfef982f4f5eace0005
Al Busaidi <MU63699@corp.pdo.om>
2025-06-04 14:26:13 +0400

    Create test file

---
+++ heredity/test_heredity.py
@@ -0,0 +1,8 @@
+from heredity import *
+
+print()
+print(f"PROBS['gene']: {PROBS['gene']}")
+print()
+print(f"PROBS['trait']: {PROBS['trait']}")
+print()
+print(f"PROBS['mutation']: {PROBS['mutation']}")
