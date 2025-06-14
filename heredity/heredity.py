53ce5ded8739149a0c40214a0c32a2a7411beb56
Al Busaidi <MU63699@corp.pdo.om>
2025-06-14 18:15:27 +0400

    Implement update -- NOT TESTED

--- heredity/heredity.py
+++ heredity/heredity.py
@@ -182,7 +182,13 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
     Which value for each distribution is updated depends on whether
     the person is in `have_gene` and `have_trait`, respectively.
     """
-    raise NotImplementedError
+    
+    for person in probabilities:
+        if person in two_genes:   probabilities[person]["gene"][2] += p
+        elif person in one_gene:  probabilities[person]["gene"][1] += p
+        else:                     probabilities[person]["gene"][0] += p
+
+        probabilities[person]["trait"][person in have_trait] += p
 
 
 def normalize(probabilities):
