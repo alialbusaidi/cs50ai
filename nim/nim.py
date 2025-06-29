416b00ed8bde74782b89459b2f1fb4d64c007172
alialbusaidi <ali.busaidi@gmail.com>
2025-06-29 09:02:32 +0400

    Draft update_q_value

--- nim/nim.py
+++ nim/nim.py
@@ -118,7 +118,9 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        raise NotImplementedError
+        new_q = old_q + self.alpha * (self.get_q_value(state, action) - old_q)
+        
+        return new_q
 
     def best_future_reward(self, state):
         """
